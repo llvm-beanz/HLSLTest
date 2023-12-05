4b4deff14a8989a779b4b93dd0f5ae5e0a02c3bd
Chris B <cbieneman@microsoft.com>
2023-12-05 15:46:43 -0600

    Query Vulkan devices

--- lib/API/VK/Device.cpp
+++ lib/API/VK/Device.cpp
@@ -9,8 +9,87 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "HLSLTest/API/Device.h"
 #include "llvm/Support/Error.h"
 
-llvm::Error InitializeVXDevices() {
-  return llvm::Error::success();
-}
+#include <memory>
+#include <vulkan/vulkan.h>
+
+using namespace hlsltest;
+
+namespace {
+
+class VKDevice : public hlsltest::Device {
+private:
+  VkPhysicalDevice Device;
+  VkPhysicalDeviceProperties Props;
+  Capabilities Caps;
+
+public:
+  VKDevice(VkPhysicalDevice D) : Device(D) {
+    vkGetPhysicalDeviceProperties(Device, &Props);
+  }
+  VKDevice(const VKDevice &) = default;
+
+  ~VKDevice() override = default;
+
+  llvm::StringRef getAPIName() const override { return "Vulkan"; }
+  llvm::StringRef getDescription() const { return Props.deviceName; }
+
+  const Capabilities &getCapabilities() override { return Caps; }
+};
+
+class VKContext {
+private:
+  VkInstance Instance;
+  llvm::SmallVector<std::shared_ptr<VKDevice>> Devices;
+
+  VKContext() = default;
+  ~VKContext() { vkDestroyInstance(Instance, NULL); }
+  VKContext(const VKContext &) = delete;
+
+public:
+  static VKContext &instance() {
+    static VKContext Ctx;
+    return Ctx;
+  }
+
+  llvm::Error initialize() {
+    VkApplicationInfo AppInfo = {};
+    AppInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    AppInfo.pApplicationName = "HLSLTest";
+    AppInfo.apiVersion = VK_API_VERSION_1_1;
+
+    VkInstanceCreateInfo CreateInfo = {};
+    CreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    CreateInfo.pApplicationInfo = &AppInfo;
+
+    VkResult Res = vkCreateInstance(&CreateInfo, NULL, &Instance);
+    if (Res == VK_ERROR_INCOMPATIBLE_DRIVER)
+      return llvm::createStringError(std::errc::no_such_device,
+                                     "Cannot find a compatible Vulkan device");
+    else if (Res)
+      return llvm::createStringError(std::errc::no_such_device,
+                                     "Unkown Vulkan initialization error");
+
+    uint32_t DeviceCount = 0;
+    if (vkEnumeratePhysicalDevices(Instance, &DeviceCount, nullptr))
+      return llvm::createStringError(std::errc::no_such_device,
+                                     "Failed to get device count");
+    std::vector<VkPhysicalDevice> PhysicalDevices(DeviceCount);
+    if (vkEnumeratePhysicalDevices(Instance, &DeviceCount,
+                                   PhysicalDevices.data()))
+      return llvm::createStringError(std::errc::no_such_device,
+                                     "Failed to enumerate devices");
+    for (const auto &Dev : PhysicalDevices) {
+      auto NewDev = std::make_shared<VKDevice>(Dev);
+      Devices.push_back(NewDev);
+      Device::registerDevice(std::static_pointer_cast<Device>(NewDev));
+    }
+
+    return llvm::Error::success();
+  }
+};
+} // namespace
+
+llvm::Error InitializeVXDevices() { return VKContext::instance().initialize(); }
