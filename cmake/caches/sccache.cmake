a8d9117cab312ad1b503af913038712fec5be879
Chris Bieneman <chris.bieneman@me.com>
2024-12-01 19:23:48 -0600

    Workflow maybe on Windows? - take 1010

--- cmake/caches/sccache.cmake
+++ cmake/caches/sccache.cmake
@@ -1,5 +1,9 @@
-find_program(SCCACHE sccache HINTS /usr/local/bin)
-if (SCCACHE)
-  set(CMAKE_C_COMPILER_LAUNCHER ${SCCACHE} CACHE STRING "")
-  set(CMAKE_CXX_COMPILER_LAUNCHER ${SCCACHE} CACHE STRING "")
+# I'm having trouble getting sccache to work on windows, so for now skip it
+# unless we're on Unix.
+if (UNIX)
+  find_program(SCCACHE sccache HINTS /usr/local/bin)
+  if (SCCACHE)
+    set(CMAKE_C_COMPILER_LAUNCHER ${SCCACHE} CACHE STRING "")
+    set(CMAKE_CXX_COMPILER_LAUNCHER ${SCCACHE} CACHE STRING "")
+  endif()
 endif()
