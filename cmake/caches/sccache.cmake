5055de5513ba87e2f530b157a3b0d19309ba6791
Chris Bieneman <chris.bieneman@me.com>
2024-12-01 20:36:27 -0600

    Workflow maybe on Windows? - take 1011

--- cmake/caches/sccache.cmake
+++ cmake/caches/sccache.cmake
@@ -1,9 +1,9 @@
-# I'm having trouble getting sccache to work on windows, so for now skip it
-# unless we're on Unix.
-if (UNIX)
-  find_program(SCCACHE sccache HINTS /usr/local/bin)
-  if (SCCACHE)
-    set(CMAKE_C_COMPILER_LAUNCHER ${SCCACHE} CACHE STRING "")
-    set(CMAKE_CXX_COMPILER_LAUNCHER ${SCCACHE} CACHE STRING "")
-  endif()
+find_program(SCCACHE sccache HINTS /usr/local/bin)
+if (SCCACHE)
+  set(CMAKE_POLICY_DEFAULT_CMP0141 NEW CACHE STRING "")
+  set(CMAKE_MSVC_DEBUG_INFORMATION_FORMAT "ProgramDatabase")
+  set(CMAKE_CXX_COMPILER_FLAGS "/FS")
+  set(CMAKE_C_COMPILER_FLAGS "/FS")
+  set(CMAKE_C_COMPILER_LAUNCHER ${SCCACHE} CACHE STRING "")
+  set(CMAKE_CXX_COMPILER_LAUNCHER ${SCCACHE} CACHE STRING "")
 endif()
