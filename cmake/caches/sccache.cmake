b72f5b04bcdf6838f60a38cf1fd2108f92fa03c8
Chris Bieneman <chris.bieneman@me.com>
2024-12-01 19:04:24 -0600

    Workflow maybe on Windows? - take 1007

--- cmake/caches/sccache.cmake
+++ cmake/caches/sccache.cmake
@@ -1,3 +1,7 @@
+if(POLICY CMP0141)
+  cmake_policy(SET CMP0141 NEW)
+endif()
+
 find_program(SCCACHE sccache HINTS /usr/local/bin)
 if (SCCACHE)
   set(CMAKE_C_COMPILER_LAUNCHER ${SCCACHE} CACHE STRING "")
